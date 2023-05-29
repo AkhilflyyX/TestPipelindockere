963ee3f87e53156605de1ca655e40360459cc151
AkhilflyyX <achekz25@gmail.com>
2023-05-29 19:11:21 +0530

    test commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.akhilrajk389.dockertest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
